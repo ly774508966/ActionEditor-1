b26fbba842eb33db18831e16749a1d5d50571200
Akari <149439786@qq.com>
2021-06-07 17:56:04 +0800

    Playable扩展

---
+++ Assets/PlayerCtrl.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using static SimpleAnimationPlayable;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    public StateInfoScriptableObject stateInfoScriptable;
+
+    public SimpleAnimation simpleAnimation;
+    public float gameSpeed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        simpleAnimation.AddState(stateInfoScriptable);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [Button("Set Game Speed")]
+    public void SetGameSpeed()
+    {
+        Time.timeScale = gameSpeed;
+    }
+
+    [Button("Play Idle")]
+    public void PlayIdle()
+    {
+        simpleAnimation.Play("Default");
+    }
+
+    [Button("Play Walk")]
+    public void PlayWalk()
+    {
+        simpleAnimation.Play("Walk");
+    }
+
+    [Button("Play Run")]
+    public void PlayRun()
+    {
+        simpleAnimation.Play("Run");
+    }
+
+    [Button("Play Attack")]
+    public void PlayAttack()
+    {
+        simpleAnimation.Play("Attack");
+    }
+
+    [Button("Play Dash")]
+    public void PlayDash()
+    {
+        simpleAnimation.Play("Dash");
+    }
+
+    [Button("Play RunFast")]
+    public void PlayRunFast()
+    {
+        simpleAnimation.Play("RunFast");
+    }
+
+
+    public float time = 0.2f;
+    [Button("CrossFade Attack")]
+    public void CrossFadeAttack()
+    {
+        simpleAnimation.CrossFade("Attack", time);
+    }
+}
---
+++ Assets/StateInfoScriptableObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObject/StateInfo",fileName ="New StateInfo",order = 0)]
+public class StateInfoScriptableObject : ScriptableObject
+{
+    public string stateName;
+    public AnimationClip clip;
+    public WrapMode wrapMode;
+
+    public AnimationClipEvent[] animationEvents;
+
+}
+
+[Serializable]
+public class AnimationClipEvent
+{
+    public int frameIndex = 0;
+    public Enum_AnimationClipEvent type;
+}
+
+public enum Enum_AnimationClipEvent
+{
+    None = 0,
+    AnimationEnd = 1,
+}
